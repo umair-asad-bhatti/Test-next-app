ccb2f5928f6d675f87eeb55993a5a6072a500d66
umair-asad-bhatti <umaircyber6@gmail.com>
2023-08-28 19:43:29 +0500

    modified the body background color and styled the form and inputs fileds

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,5 +1,6 @@
 
 'use client'
+import Link from 'next/link'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { joiResolver } from '@hookform/resolvers/joi'
@@ -35,16 +36,16 @@ const Page = () => {
     }
     return (
         <div className='p-2 m-2  flex flex-col items-center  justify-center '>
-            <h1 className='text-5xl my-4 text-blue-500'>Login</h1>
-            <form className='p-4 shadow-lg border border-slate-200' onSubmit={handleSubmit((onLoginSubmit))} style={{ "max-width": "400px", "width": "400px" }}>
+            <h1 className='text-5xl my-4 text-blue-400'>Login</h1>
+            <form className='p-4 shadow-lg border bg-white border-slate-200' onSubmit={handleSubmit((onLoginSubmit))} style={{ "max-width": "400px", "width": "400px" }}>
                 <div>
                     <label className='block py-2 text-slate-500' htmlFor="email">Email</label>
-                    <input id='email' placeholder='Email*' className='border-blue-400  block w-[100%] border outline-blue-500 px-2 py-1 rounded'  {...register('email')} />
+                    <input id='email' placeholder='Email*' className='border-blue-400 bg-zinc-50 block w-[100%] border outline-blue-500 px-2 py-1 rounded'  {...register('email')} />
                     {errors.email && <p className='text-red-400'>{errors.email.message}</p>}
                 </div >
                 <div>
                     <label className='block py-2 text-slate-500' htmlFor="password">Password</label>
-                    <input type='password' placeholder='Password*' id="password" className='border-blue-400  block w-[100%] border outline-blue-500 px-2 py-1 rounded'  {...register('password')} />
+                    <input type='password' placeholder='Password*' id="password" className='bg-zinc-50 border-blue-400  block w-[100%] border outline-blue-500 px-2 py-1 rounded'  {...register('password')} />
                     {errors.password && <p className='text-red-400'>{errors.password.message}</p>}
                 </div>
                 {
@@ -52,6 +53,9 @@ const Page = () => {
                         <input disabled className='block w-[100%]  bg-slate-500 px-2 py-1 my-4 text-white text-center' value="Logging in..." />
                         : <input className='block w-[100%] hover:bg-blue-600 cursor-pointer bg-blue-500 px-2 py-1 my-4 text-white shaodw rounded' type="submit" value="Log in" />
                 }
+                <div className='text-center m-2 p-2'>
+                    <h4>Dont have account?<Link className='text-blue-500 underline' href={"/register"}> Register here</Link></h4>
+                </div>
             </form >
         </div >
     )
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,6 +1,9 @@
 'use client'
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import Link from 'next/link';
+import alertify from 'alertifyjs';
+import 'alertifyjs/build/css/alertify.css';
 import { joiResolver } from '@hookform/resolvers/joi';
 import schema from '@/helper/joiregisterschema';
 const Page = () => {
@@ -30,6 +33,7 @@ const Page = () => {
             setError(server_data.errors.context.key, { type: server_data.errors.type, message: server_data.errors.message });
             return;
         }
+        alertify.success('Registered Successfully')
         console.log(server_data);
         reset()
         return;
@@ -38,31 +42,31 @@ const Page = () => {
         <div className='p-2 m-2  flex flex-col items-center  justify-center '>
 
             <h1 className='text-5xl my-4 text-blue-500'>Register Here</h1>
-            <form className='p-4 shadow-lg border md:w-[400px] lg:w-[500px] w-auto border-slate-200' onSubmit={handleSubmit(onSubmit)}>
+            <form className='p-4 shadow-lg border bg-white md:w-[400px] lg:w-[500px] w-auto border-slate-200' onSubmit={handleSubmit(onSubmit)}>
                 <div>
                     <label className='block py-2 text-slate-500' htmlFor="">First Name</label>
-                    <input id='firstname' placeholder='first name*' className='  block w-[100%] border outline-blue-500 px-2 py-1 rounded'  {...register('firstname')} />
+                    <input id='firstname' placeholder='first name*' className='bg-zinc-50  block w-[100%] border outline-blue-500 px-2 py-1 rounded'  {...register('firstname')} />
                     {errors.firstname && <p className='text-red-400'>{errors.firstname.message}</p>}
                 </div >
                 <div>
                     <label className='block py-2 text-slate-500' htmlFor="lastname">Last Name</label>
-                    <input placeholder='last name*' id="lastname" className='  block w-[100%] border outline-blue-500 px-2 py-1 rounded'  {...register('lastname')} />
+                    <input placeholder='last name*' id="lastname" className='bg-zinc-50  block w-[100%] border outline-blue-500 px-2 py-1 rounded'  {...register('lastname')} />
                     {errors.lastname && <p className='text-red-400'>{errors.lastname.message}</p>}
 
                 </div>
                 <div>
                     <label className='block py-2 text-slate-500' htmlFor="email">Email</label>
-                    <input placeholder='email*' id="email" className=' border  block w-[100%] outline-blue-500 px-2 py-1 rounded'  {...register('email')} />
+                    <input placeholder='email*' id="email" className='bg-zinc-50 border  block w-[100%] outline-blue-500 px-2 py-1 rounded'  {...register('email')} />
                     {errors.email && <p className='text-red-400'>{errors.email.message}</p>}
                 </div>
                 <div>
                     <label className='block py-2 text-slate-500' htmlFor="password">Password</label>
-                    <input type='password' placeholder='password*' id="password" className=' border block w-[100%]  outline-blue-500 px-2 py-1 rounded'  {...register('password')} />
+                    <input type='password' placeholder='password*' id="password" className='bg-zinc-50 border block w-[100%]  outline-blue-500 px-2 py-1 rounded'  {...register('password')} />
                     {errors.password && <p className='text-red-400'>{errors.password.message}</p>}
                 </div>
                 <div>
                     <label className='block py-2 text-slate-500' htmlFor="confirm password">Confirm Password</label>
-                    <input placeholder='Confirm password*' type='password' id="confirm password" className=' border  block w-[100%] outline-blue-500 px-2 py-1 rounded'  {...register('confirm_password')} />
+                    <input placeholder='Confirm password*' type='password' id="confirm password" className='bg-zinc-50 border  block w-[100%] outline-blue-500 px-2 py-1 rounded'  {...register('confirm_password')} />
                     {errors.confirm_password && <p className='text-red-400 '>{errors.confirm_password.message}</p>}
 
                 </div>
@@ -72,7 +76,10 @@ const Page = () => {
                         :
                         <input className='block w-[100%] hover:bg-blue-600 cursor-pointer bg-blue-500 px-2 py-1 my-4 text-white shaodw rounded' type="submit" value={'submit'} />
                 }
-
+                <hr/>
+                <div className='text-center m-2 p-2'>
+                    <h4>Already registerd? <Link className='text-blue-500 underline' href={"/login"}>Login</Link></h4>
+                </div>
             </form >
         </div >
     )
